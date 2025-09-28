33029a31e346db96b4f7d50fac4754cbf7aa3d62
Tutyonick-Oscar <tutyonickoscar367@gmsail.com>
2025-09-28 02:58:27 +0200

    fix converter undefined variable

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\ViewsController;
 use App\Models\Member;
 use App\Models\Post;
 use App\Models\Testimonial;
+use League\CommonMark\CommonMarkConverter;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    $converter = new CommonMarkConverter();
     return view('site.index',[
         'members' => Member::all(),
+        'converter'=>$converter,
         'testimonials'=>Testimonial::orderBy('created_at','desc')->limit(4)->get(),
         'latest_posts'=>Post::with('user')->orderBy('created_at','desc')->limit(3)->get(),
     ]);
